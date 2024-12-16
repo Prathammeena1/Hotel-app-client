587903918b84bb77eddefefd71fdabc23bed6b09
Pratham meena <116185659+Prathammeena1@users.noreply.github.com>
2024-12-16 23:01:47 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,7 @@ const Login = ({display,setDisplay}) => {
         formState: { errors },
     } = useForm();
 
+    const onSubmit = ()=>{};
 
   if (!display) return null; // If display is false, do not render the component
 
